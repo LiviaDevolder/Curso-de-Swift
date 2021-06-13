337cc8da8ea656a0de755bab3b970d2a532c0ddf
LiviaDevolder <liviadevolder@gmail.com>
2021-06-13 13:50:02 -0300

    Create Array.swift

---
+++ Array.swift
@@ -0,0 +1,8 @@
+import Swift
+
+let vetNomes = ["Livia", "Martha", "Rebecca"]
+let vetIdades = [18, 19, 29]
+
+for i in 0 ..< vetNomes.count{
+    print("A \(i+1)a pessoa tem o nome \(vetNomes[i]) e a idade \(vetIdades[i])")
+}
